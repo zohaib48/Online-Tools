238413703e5e45767b2059936c8d6c301147f689
zohaib <zohaibshafique48@gmail.com>
2023-11-06 13:19:06 -0800

    Add Site Status Tool

---
+++ src/main/java/com/example/demo/statusCodeController.java
@@ -0,0 +1,51 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import com.example.demo.models.StatusCodeResult;
+
+@Controller
+public class statusCodeController {
+
+    @GetMapping("/code")
+    public String home() {
+        return "statusCode";
+    }
+
+    @GetMapping("/check-status")
+    public String checkStatus() {
+        return "checkStatus";
+    }
+
+    @RestController
+    public class StatusCodeController {
+    
+        @PostMapping("/check-status")
+        public ResponseEntity<StatusCodeResult> checkStatus(@RequestParam("urls") String url) {
+            try {
+                System.out.println(url);
+                URL websiteURL = new URL(url);
+                HttpURLConnection connection = (HttpURLConnection) websiteURL.openConnection();
+                connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36");
+                connection.setRequestMethod("GET");
+                int statusCode = connection.getResponseCode();
+                StatusCodeResult result = new StatusCodeResult(url, statusCode);
+                return new ResponseEntity<>(result, HttpStatus.OK);
+            } catch (IOException e) {
+                e.printStackTrace();
+                return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        }
+    }
+}
