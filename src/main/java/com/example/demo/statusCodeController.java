d02fca358fb14280c786e0da9dabbee59d8e9d69
zohaib <zohaibshafique48@gmail.com>
2023-11-11 12:37:15 -0800

    complete status code without frontend

--- src/main/java/com/example/demo/statusCodeController.java
+++ src/main/java/com/example/demo/statusCodeController.java
@@ -1,15 +1,15 @@
 package com.example.demo;
 
+// Import statements...
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import com.example.demo.models.StatusCodeResult;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
@@ -18,48 +18,87 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import com.example.demo.models.StatusCodeResult;
 
 @Controller
 public class statusCodeController {
-    private List<String> liveUpdates = new ArrayList<>();
+
+    private static final int MAX_WEBSITES_TO_CHECK = 20;
+
     private Map<Integer, Integer> statusCounts = new HashMap<>();
+    private List<StatusCodeResult> results = new ArrayList<>();
+    private ExecutorService executorService;
 
     @GetMapping("/code")
     public String home() {
+        statusCounts = new HashMap<>();
+        results = new ArrayList<>();
         return "statusCode";
     }
 
     @GetMapping("/check-status")
     public String checkStatus() {
+        // No need to initialize statusCounts and results here, as they are initialized in checkStatus method
         return "checkStatus";
     }
 
     @PostMapping("/check-status")
-    public String checkStatus(@RequestParam("sitemapUrl") String sitemapUrl) {
-        try {
-            liveUpdates.clear();
-            statusCounts.clear();
-            Document doc = Jsoup.connect(sitemapUrl).get();
-            List<StatusCodeResult> results = new ArrayList<>();
+    public String checkStatus(@RequestParam("sitemapUrl") String sitemapUrl,
+                              @RequestParam(value = "numThreads", defaultValue = "5") int numThreads,
+                              Model model) {
+        // Initialize statusCounts and results here
+        statusCounts = new HashMap<>();
+        results = new ArrayList<>();
 
-            for (Element urlElement : doc.select("urlset > url > loc")) {
+        try {
+            Document doc = Jsoup.connect(sitemapUrl).get();
+
+            // Ensure that the number of websites to check does not exceed the maximum limit
+            int websitesToCheck = Math.min(MAX_WEBSITES_TO_CHECK, doc.select("urlset > url > loc").size());
+
+            // Create a thread pool with the specified number of threads
+            executorService = Executors.newFixedThreadPool(numThreads);
+
+            for (Element urlElement : doc.select("urlset > url > loc").subList(0, websitesToCheck)) {
                 String url = urlElement.text();
-                int statusCode = getStatusCode(url);
-                results.add(new StatusCodeResult(url, statusCode));
-                updateStatusCounts(statusCode);
-                addLiveUpdate("Processed URL: " + url + " - Status Code: " + statusCode);
-                
-               
-         
+
+                // Execute the status checking task in a separate thread
+                executorService.execute(() -> {
+                    try {
+                        int statusCode = getStatusCode(url);
+                        results.add(new StatusCodeResult(url, statusCode));
+                        updateStatusCounts(statusCode);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        // Handle the error as needed
+                    }
+                });
             }
 
+            // Shutdown the thread pool and wait for tasks to complete
+            executorService.shutdown();
+            executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+
+            // Add the results and limit status to the model for display in the frontend
+            model.addAttribute("limitReached", websitesToCheck == MAX_WEBSITES_TO_CHECK);
+
+            System.out.println("program not ending");
+
             return "statusCode";
-        } catch (IOException e) {
+        } catch (IOException | InterruptedException e) {
             e.printStackTrace();
             // Handle the error as needed
-          
+        } finally {
+            // Ensure that the thread pool is shut down even if an exception occurs
+            if (executorService != null) {
+                executorService.shutdown();
+            }
         }
-         return "statusCode";
+
+        return "statusCode";
     }
 
     private int getStatusCode(String url) throws IOException {
@@ -74,17 +113,20 @@ public class statusCodeController {
         statusCounts.put(statusCode, statusCounts.getOrDefault(statusCode, 0) + 1);
     }
 
-    private void addLiveUpdate(String update) {
-        liveUpdates.add(update);
-    }
-
-      @GetMapping("/score")
-    public ResponseEntity <Map<Integer, Integer>> getscore() {
+    @GetMapping("/score")
+    public ResponseEntity<Map<Integer, Integer>> getscore() {
         return new ResponseEntity<>(statusCounts, HttpStatus.OK);
     }
 
-    @GetMapping("/live-updates")
-    public ResponseEntity<List<String>> getLiveUpdates() {
-        return new ResponseEntity<>(liveUpdates, HttpStatus.OK);
+    @GetMapping("/urls/{statusCode}")
+    public ResponseEntity<List<String>> getUrlsByStatusCode(@PathVariable int statusCode) {
+        List<String> urls = new ArrayList<>();
+        // Iterate over the results and add URLs with the specified status code
+        for (StatusCodeResult result : results) {
+            if (result.getStatusCode() == statusCode) {
+                urls.add(result.getUrl());
+            }
+        }
+        return new ResponseEntity<>(urls, HttpStatus.OK);
     }
 }
